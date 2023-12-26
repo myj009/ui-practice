ddced420e17586748877105f66f5d5f9f796c578
myj009 <mohammad.jagora@deliveroo.com>
2023-12-26 19:34:56 +0530

    Adding docker file

--- ebex-ui/api/index.ts
+++ ebex-ui/api/index.ts
@@ -7,5 +7,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/user", userRouter);
+const port = process.env.PORT || 3001;
 
-app.listen(3001, () => console.log("server is running"));
+app.listen(port, () => console.log(`server is running on port ${port}`));
