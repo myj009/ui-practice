7585a4b369db5884eaad020d67c050737334cc63
myj009 <mohammad.jagora@deliveroo.com>
2023-12-26 22:08:30 +0530

    Adding test get path

--- ebex-ui/api/index.ts
+++ ebex-ui/api/index.ts
@@ -7,6 +7,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/user", userRouter);
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello world");
+});
 const port = process.env.PORT || 3001;
 
 app.listen(port, () => console.log(`server is running on port ${port}`));
